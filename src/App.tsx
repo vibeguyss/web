a6b0fc0872ad6c83df26b202cc6162a95ade8c50
bbqcream <imground11@gmail.com>
2025-07-15 12:15:05 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white text-4xl font-bold">
+            Tailwind CSS가 잘 적용되고 있습니다!
+        </div>
+    );
+}
